bf9a19b979eea4238758f40b791ad1eb901de2e7
Nghi Le <vinhnghi223@gmail.com>
2017-03-13 00:33:49 +0200

    Added code coverage

--- karma.conf.js
+++ karma.conf.js
@@ -1,38 +1,30 @@
 //jshint strict: false
 module.exports = function(config) {
   config.set({
-
     basePath: '.',
-
+    autoWatch: true,
+    frameworks: ['jasmine', 'browserify'],
     files: [
       'node_modules/moment/moment.js',
       'node_modules/angular/angular.js',
       'node_modules/angular-mocks/angular-mocks.js',
       'src/**/*.js'
     ],
-
-    autoWatch: true,
-
-    frameworks: ['jasmine', 'browserify'],
-    preprocessors: {
-      'src/**/*.js': [ 'browserify' ]
-    },
-
     browsers: ['Chrome'],
+    reporters: ['progress', 'coverage'],
+    preprocessors: { 'src/**/*.js': ['browserify','coverage'] },
+    singleRun: true,
 
     plugins: [
-      //'karma-requirejs',
+      'requirejs',
+      'karma-requirejs',
+      'karma-coverage',
       'karma-browserify',
       'karma-chrome-launcher',
       'karma-firefox-launcher',
       'karma-jasmine',
-      'karma-junit-reporter'
-    ],
-
-    junitReporter: {
-      outputFile: 'test_out/unit.xml',
-      suite: 'unit'
-    }
+      //'karma-junit-reporter'
+    ]
 
   });
 };
