57ffd5bfb1bc9bc2f18d6dd92ae35737a7426325
Nghi Le <vinhnghi223@gmail.com>
2017-03-17 22:25:07 +0200

    Fixed test coverage

--- karma.conf.js
+++ karma.conf.js
@@ -6,25 +6,29 @@ module.exports = function(config) {
     frameworks: ['jasmine', 'browserify'],
     files: [
       'node_modules/moment/moment.js',
-      'node_modules/angular/angular.js',
-      'node_modules/angular-mocks/angular-mocks.js',
-      'src/**/*.js'
+      'src/*.js'
     ],
     browsers: ['Chrome'],
     reporters: ['progress', 'coverage'],
-    preprocessors: { 'src/**/*.js': ['browserify','coverage'] },
+    preprocessors: {
+      'src/*.js': ['browserify']
+    },
+    browserify: {
+      debug: true,
+      transform: ['browserify-istanbul']
+    },
+    coverageReporter: {
+      reporters : [
+        {"type": "text"},
+        {"type": "html", dir: 'coverages'}
+      ]
+    },
     singleRun: true,
-
     plugins: [
-      'requirejs',
-      'karma-requirejs',
       'karma-coverage',
       'karma-browserify',
       'karma-chrome-launcher',
-      'karma-firefox-launcher',
-      'karma-jasmine',
-      //'karma-junit-reporter'
+      'karma-jasmine'
     ]
-
   });
 };
